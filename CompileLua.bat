1dcd808b322472ec7e0455116c7649cf0c440438
memcorrupt <41022155+memcorrupt@users.noreply.github.com>
2024-11-08 13:49:53 -0500

    try #4

--- CompileLua.bat
+++ CompileLua.bat
@@ -1,5 +1,8 @@
 ::
-:: Compiles Lua (from https://github.com/Pharap/CompilingLua/blob/master/Compile.bat)
+:: Compiles Lua
+:: this file is based on https://github.com/Pharap/CompilingLua/blob/master/Compile.bat
+:: and contains additional modifications suited to the CrossLua project
+:: *THIS ONLY BUILDS LUA CLI! COMPILER IS NOT INCLUDED.*
 ::
 
 ::
@@ -8,21 +11,6 @@
 
 :: Start local variable scope
 @SETLOCAL
-
-:: Locate 'vcvarsall.bat'
-@IF NOT "%VS120COMNTOOLS%"=="" @SET VSVARSALL=%VS120COMNTOOLS%..\..\VC\vcvarsall.bat
-@IF NOT "%VS140COMNTOOLS%"=="" @SET VSVARSALL=%VS140COMNTOOLS%..\..\VC\vcvarsall.bat
-@IF "%VSVARSALL%"=="" @GOTO ENDSETUP
-
-:: Identify the target architecture
-@IF NOT "%PROCESSOR_ARCHITECTURE%"=="" (
-	@IF "%PROCESSOR_ARCHITECTURE%"=="x86" @SET ARCH=x86
-	@IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" @SET ARCH=amd64
-)
-@IF "%ARCH%"=="" @GOTO ENDSCRIPT
-
-:: Call the setup script
-@CALL "%VSVARSALL%" %ARCH%
 @ECHO ON
 
 :ENDSETUP
@@ -31,9 +19,6 @@
 :: Process files
 ::
 
-:: Move down into 'src'
-@PUSHD src
-
 :: Clean up files from previous builds
 @IF EXIST *.o @DEL *.o
 @IF EXIST *.obj @DEL *.obj
@@ -41,31 +26,14 @@
 @IF EXIST *.exe @DEL *.exe
 
 :: Compile all .c files into .obj
-@CL /MD /O2 /c /DLUA_BUILD_AS_DLL *.c
+@CL /nologo /MT /O2 /W3 /GL /c /D_CRT_SECURE_NO_DEPRECATE *.c
 
 :: Rename two special files
 @REN lua.obj lua.o
 @REN luac.obj luac.o
 
-:: Link up all the other .objs into a .lib and .dll file
-@LINK /DLL /IMPLIB:lua.lib /OUT:lua.dll *.obj
-
 :: Link lua into an .exe
-@LINK /OUT:lua.exe lua.o lua.lib
-
-:: Create a static .lib
-@LIB /OUT:lua-static.lib *.obj
-
-:: Link luac into an .exe
-@LINK /OUT:luac.exe luac.o lua-static.lib
-
-:: Move back up out of 'src'
-@POPD
-
-:: Copy the library and executable files out from 'src'
-@COPY /Y src\lua.exe lua.exe
-@COPY /Y src\luac.exe luac.exe
-@COPY /Y src\lua.dll lua.dll
+@LINK /nologo /LTCG /INCREMENTAL:NO /OUT:lua.exe lua.o *.obj
 
 :ENDSCRIPT
 
